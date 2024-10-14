87bb2a5754799c75459a1bf3cae9d4d072e8c0cb
Moshoeu Khwinana <moshkhwinana@gmail.com>
2024-10-14 17:36:32 +0200

    completed contact messages - finally!

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,11 @@
 class ContactMailer < ApplicationMailer
   def send_contact_message(contact_message)
     @contact_message = contact_message
-    mail(to: ENV['EMAIL'], from: @contact_message.email, subject: "New message from #{@contact_message.name}")
+    mail(
+      to: ENV['EMAIL'],
+      from: @contact_message.email,
+      reply_to: @contact_message.email,
+      subject: "New message from #{@contact_message.name}"
+    )
   end
 end
