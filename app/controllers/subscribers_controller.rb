fcf62b789881a4f80c79f4e2d8a616313089d62c
Moshoeu Khwinana <moshkhwinana@gmail.com>
2024-11-13 17:55:33 +0200

    flash notice

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,7 +3,7 @@ class SubscribersController < ApplicationController
     # no @ instance variables because NO need to pass data to the view files
     mailchimp_service = MailchimpService.new
     mailchimp_service.subscribe(params[:subscriber][:email], params[:subscriber][:name], params[:subscriber][:surname])
-    flash.now[:notice] = 'You have been added to our mailing list!'
+    flash[:notice] = 'You have been added to our mailing list!'
     redirect_to root_path
   end
 end
